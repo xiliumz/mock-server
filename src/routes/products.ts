0462e8c960e851fab10d8a8272898af856f3f70f
Azaria Beryl <beryl.naga@gmail.com>
2025-03-30 15:54:38 +0700

    feat: enhance product route
    with dynamic data generation and pagination support
    
    Updated the product index and show routes to utilize the faker library for generating dynamic mock data. Increased the number of products returned in the index route to 50 and added query parameters for pagination, allowing for offset and limit adjustments in the response.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,30 +1,46 @@
 import createRoute from '../helpers/create-routes';
-
+import { faker } from '@faker-js/faker';
 export const index = createRoute({
   path: '/products',
   method: 'get',
   response: {
-    products: Array.from({ length: 25 }).map(() => ({
-      id: 'uuid',
-      name: 'lorem.2.4',
-      description: 'lorem.5.10',
-      price: 'number.1.100',
+    products: Array.from({ length: 50 }).map(() => ({
+      id: faker.string.uuid(),
+      name: faker.lorem.words(2),
+      description: faker.lorem.sentence(5),
+      price: faker.number.int({ min: 1, max: 100 }),
     })),
   },
+  queryParams: [
+    {
+      name: 'offset',
+      handler: (data, value = '0') => {
+        data.products = data.products.slice(parseInt(value));
+      },
+    },
+    {
+      name: 'limit',
+      handler: (data, value = '10') => {
+        const products = data.products.slice(0, parseInt(value));
+        console.log(products);
+        data.products = products;
+      },
+    },
+  ],
 });
 
 export const show = createRoute({
   path: '/products/:id',
   method: 'get',
   response: {
-    id: 'uuid',
-    name: 'lorem.2.4',
-    description: 'lorem.5.10',
-    price: 'number.1.100',
+    id: faker.string.uuid(),
+    name: faker.lorem.words(2),
+    description: faker.lorem.sentence(5),
+    price: faker.number.int({ min: 1, max: 100 }),
     company: {
-      id: 'uuid',
-      name: 'lorem.2.4',
-      description: 'lorem.5.10',
+      id: faker.string.uuid(),
+      name: faker.lorem.words(2),
+      description: faker.lorem.sentence(5),
     },
   },
 });
