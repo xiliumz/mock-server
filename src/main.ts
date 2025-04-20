e04e6b99b7e9f06011f5d64411cac9ac17ff2d00
azariaberyl <beryl.naga@gmail.com>
2025-04-20 18:16:31 +0700

    feat: integrate route transformation into server setup
    - Updated the server initialization to utilize the new `transformRoutesToPathBased` function, allowing for a more structured route registration process. This change enhances the clarity and maintainability of route handling in the application.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import buildApp from './app';
 import * as routes from './routes';
 import Route from './types/route';
+import { transformRoutesToPathBased } from './helpers/transform-routes';
+
+const routesArray = Object.values(routes);
+const routesByPath = transformRoutesToPathBased(routesArray);
 
 // Create server
-const app = buildApp(Object.values(routes) as unknown as Route<any>[]);
+const app = buildApp(Object.values(routesByPath) as Route<any>[]);
 
 app.listen(3000, () => {
   console.log('Mock Server running at http://localhost:3000 🚀');
